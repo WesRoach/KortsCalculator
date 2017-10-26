180e3ad0d404da9ebb8c6a45cfcb6fe822cea113
Aaron Tomason <artomason@gmail.com>
2017-10-26 07:36:31 -0400

    Cleaned up source code

--- tuple2.py
+++ tuple2.py
@@ -3,13 +3,10 @@
 # Granted 2006 by Ehrayn to the public domain
 #
 
-import sys
-import types
-
 __all__ = ['t2']
 
+
 class t2(tuple):
-    "A tuple object supporting list-like count() and index() methods"
 
     def __repr__(self):
         if len(self) == 0: return "t2()"
@@ -18,13 +15,13 @@ class t2(tuple):
     def count(self, value):
         cnt = 0
         for item in tuple.__iter__(self):
-            if cmp(item, value) == 0:
+            if (item > value) - (item < value):
                 cnt = cnt + 1
         return cnt
 
     def index(self, value, start=0, stop=None):
         if stop is None: stop = len(self)
-        for n in xrange(start, stop):
-            if cmp(self[n], value) == 0:
+        for n in range(start, stop):
+            if (self[n] > value) - (self[n] < value):
                 return n
-        raise ValueError, "t2.index(x): x not in tuple"
+        raise ValueError("t2.index(x): x not in tuple")
