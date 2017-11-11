2dd482c9c547c76ba2f0f7e7fbe11b07c150c2d1
Aaron Tomason <artomason@gmail.com>
2017-11-11 14:15:51 -0500

    Fixed formatting of cost

--- SC.py
+++ SC.py
@@ -7,59 +7,71 @@
 # See NOTICE.txt for copyrights and grant of license
 
 
-from Constants import *
 from Item import *
 import re
-import string
-import sys
 
 
 def formatCost(cost):
-    plat = (cost / 10000000) % 1000
-    gold = (cost / 10000) % 1000
-    silver = (cost / 100) % 100
+    plat = (cost // 10000000) % 1000
+    gold = (cost // 10000) % 1000
+    silver = (cost // 100) % 100
     copper = cost % 100
-    str = ''
+    val = ''
+
     if plat > 0:
-        str += '%dp ' % plat
+        val += '%dp ' % plat
+
     if gold > 0 or plat > 0:
-        str += '%dg ' % gold
+        val += '%dg ' % gold
+
     if silver > 0 or gold > 0 or plat > 0:
-        str += '%ds ' % silver
-    str += '%dc' % copper
-    return str
+        val += '%ds ' % silver
+
+    val += '%dc' % copper
+    return val
 
 
 def getGemNameParts(gemname):
     # returns level, liquid prefix and dust suffix
     if gemname == '':
         return ['', '', '']
+
     gemwords = str.split(gemname, ' ', 3)
+
     if gemwords[0] not in GemNames:
         return ['', '', '']
+
     # Prefix (two words distinguish some borked gems)
     if gemwords[1] not in GemLiquids:
         dbg = "Could not find liquid " + gemwords[1] + "\r\n"
         gemwords[1] += " " + gemwords[2]
+
         if gemwords[1] not in GemLiquids:
             return ['', '', '']
+
     # Suffix (may include second prefix word) to distinguish dust
     gemwords[2] = " ".join(gemwords[2:])
+
     if gemwords[2] not in GemDusts:
         dbg = "Could not find dust " + gemwords[2] + "\r\n"
         gemwords[2] = gemwords[3]
+
         if gemwords[2] not in GemDusts:
             return ['', '', '']
+
     if len(gemwords) > 3:
         gemwords.pop()
+
     return gemwords
 
 
 def gemTypeSort(a, b):
     if MaterialGems.index(a) < MaterialGems.index(b):
         return -1
+
     elif MaterialGems.index(a) > MaterialGems.index(b):
         return 1
+
     else:
         return 0
 
@@ -86,8 +98,10 @@ def gemNameSort(a, b):
 
     gemlevel_a, r = str.split(a, ' ', 1)
     gemlevel_b, r = str.split(b, ' ', 1)
+
     if GemNames.index(gemlevel_a) < GemNames.index(gemlevel_b):
         return -1
+
     elif GemNames.index(gemlevel_a) > GemNames.index(gemlevel_b):
         return 1
 
