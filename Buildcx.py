077273e29f207a7f01a9998fe1a7a7b16e388a31
Aaron Tomason <artomason@gmail.com>
2017-11-12 10:40:18 -0500

    Tweaked settings for initial build

---
+++ Buildcx.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='KortsSpellcraftingCalculator',
+    version='3.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='Kort\'s Spellcrafting Calculator'
+)
